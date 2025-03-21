d0a91e23a12278926c7ea66795165b6be574eb54
Lucas Bueno <lucasbuenno@outlook.com>
2025-03-20 21:27:27 -0300

    configurações iniciais do banco de dados e docker-file

---
+++ src/main/java/com/quack/loja_do_marreco/LojaDoMarrecoApplication.java
@@ -0,0 +1,13 @@
+package com.quack.loja_do_marreco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaDoMarrecoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaDoMarrecoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/quack/loja_do_marreco/LojaDoMarrecoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quack.loja_do_marreco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaDoMarrecoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
